d2767d8184a12a2fd9b6ea04d49d16f3f247b4b9
Marcos Albera <EXT-Marcos.Albera@irdigital.pe>
2021-07-06 13:54:00 -0300

    initial commit

---
+++ src/main/resources/db/migration/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO HERO VALUES (1,'Spiderman','Peter','Parker');
+INSERT INTO HERO VALUES (2,'Superman','Bruce','Wayne');
+INSERT INTO HERO VALUES (3,'Batman','Bruno','Diaz');
+
+INSERT INTO SKILL VALUES (1,'Sentido Aracnido','Recognitiva capacidad de sentido arácnido, se aferra a la mayoría de las superficies sólidas y la capacidad de las redes',1);
+INSERT INTO SKILL VALUES (2,'Muñequeras','Utiliza muñequeras web para disparar material de telaraña',1);
+INSERT INTO SKILL VALUES (3,'Agilidad','Fuerza, velocidad, durabilidad, agilidad, sentidos, reflejos, coordinación, equilibrio y resistencia sobrehumanos',1);
+
+INSERT INTO SKILL VALUES (4,'Fuerza','Fuerza sobrehumana',2);
+INSERT INTO SKILL VALUES (5,'poderes de visión','Rayos x, calorífica, telescópica, infrarroja, y microscópica',2);
+INSERT INTO SKILL VALUES (6,'Superoído','Superoído',2);
+INSERT INTO SKILL VALUES (7,'SuperAliento','le permite soplar a temperaturas congelantes',2);
+INSERT INTO SKILL VALUES (8,'Velocidad','Más rápido que una bala',2);
+
+INSERT INTO SKILL VALUES (9,'Acróbata','acróbata experto',3);
+INSERT INTO SKILL VALUES (10,'Intelecto','Intelecto nivel genio',3);
+INSERT INTO SKILL VALUES (11,'Estratega','Brillante estratega',3);
+
+commit;
